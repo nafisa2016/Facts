c770cb0d066d8515df27fccc1fe2b5bc5d75b9fb
nafisar <ios.nafisa@gmail.com>
2018-05-25 14:11:55 +1000

    Added model for first screen.

---
+++ Facts/FactsFeedModel.swift
@@ -0,0 +1,56 @@
+//
+//  FactsFeedModel.swift
+//  Facts
+//
+//  Created by Nafisa Rahman on 5/25/18.
+//  Copyright © 2018 com.nafisa. All rights reserved.
+//
+
+import Foundation
+
+struct FactsFeedModel : Decodable {
+    
+    var title: String
+    var rows : [Row]
+    
+    init(title: String,rows: [Row]) {
+        self.title = title
+        self.rows = rows
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case title
+        case rows
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        title = try container.decode(String.self, forKey: .title)
+        rows = try container.decode([Row].self, forKey: .rows)
+    }
+    
+}
+
+
+struct Row : Decodable {
+    var title : String?
+    var description: String?
+    var imageHref: String?
+    
+    enum CodingKeys : String, CodingKey {
+        case title
+        case description
+        case imageHref
+    }
+    
+    init(from decoder: Decoder) throws {
+        
+        let item = try decoder.container(keyedBy: CodingKeys.self)
+        
+        title = try item.decodeIfPresent(String.self,forKey: .title)
+        description = try item.decodeIfPresent(String.self,forKey: .description)
+        imageHref = try item.decodeIfPresent(String.self,forKey: .imageHref)
+    }
+}
+
+
