6421eb64b3a718fe68b56e39dd952f4399283d88
nafisar <ios.nafisa@gmail.com>
2018-05-25 16:35:46 +1000

    Added method for view model for second screen.

--- Facts/ViewModels/DetailDataViewModel.swift
+++ Facts/ViewModels/DetailDataViewModel.swift
@@ -10,4 +10,41 @@ import Foundation
 
 class DetailDataViewModel {
     
+    //MARK:- declarations
+    var detailDataModel : DetailDataModel
+    
+    init() {
+       detailDataModel = DetailDataModel(rowTitle: "", rowImgHref: "", rowDescription: "")
+    }
+    
+    //MARK:- get rowTitle
+    func getRowTitle() -> String {
+        return detailDataModel.rowTitle
+    }
+    
+    //MARK:- set rowTitle
+    func setRowTitle(title: String) {
+        detailDataModel.rowTitle = title
+    }
+    
+    //MARK:- get rowImgHref
+    func getRowImgHref() -> String {
+        return detailDataModel.rowImgHref
+    }
+    
+    //MARK:- set rowImgHref
+    func setRowImgHref(link: String) {
+        detailDataModel.rowImgHref = link
+    }
+    
+    //MARK:- get rowDescription
+    func getRowDescription() -> String {
+        return detailDataModel.rowDescription
+    }
+    
+    //MARK:- set rowDescription
+    func setRowDescription(desc: String) {
+        detailDataModel.rowDescription = desc
+    }
+     
 }
