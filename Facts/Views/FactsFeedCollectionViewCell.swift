566b8694bf62b2637a57e7b88a40eeca27480651
nafisar <ios.nafisa@gmail.com>
2018-05-25 14:58:49 +1000

    Added collection view controller and custom collection view cell.

---
+++ Facts/Views/FactsFeedCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  FactsFeedCollectionViewCell.swift
+//  Facts
+//
+//  Created by Nafisa Rahman on 5/25/18.
+//  Copyright © 2018 com.nafisa. All rights reserved.
+//
+
+import UIKit
+
+class FactsFeedCollectionViewCell: UICollectionViewCell {
+    
+}
