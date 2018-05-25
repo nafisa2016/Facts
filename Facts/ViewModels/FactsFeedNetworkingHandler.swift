3f0299d5e89eaeeefc13e67e5996de28127e88f4
nafisar <ios.nafisa@gmail.com>
2018-05-25 14:54:37 +1000

    Added files for view models for both screens.

---
+++ Facts/ViewModels/FactsFeedNetworkingHandler.swift
@@ -0,0 +1,13 @@
+//
+//  FactsFeedNetworkingHandler.swift
+//  Facts
+//
+//  Created by Nafisa Rahman on 5/25/18.
+//  Copyright © 2018 com.nafisa. All rights reserved.
+//
+
+import Foundation
+
+class FactsFeedNetworkingHandler {
+    
+}
