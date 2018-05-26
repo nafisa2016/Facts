470b07b91230896d049ac252b926c597dc5f682d
nafisar <ios.nafisa@gmail.com>
2018-05-26 21:11:02 +1000

    Supported upside down orientation.

---
+++ Facts/Extension/NavExtension.swift
@@ -0,0 +1,19 @@
+//
+//  NavExtension.swift
+//  Facts
+//
+//  Created by Nafisa Rahman on 5/26/18.
+//  Copyright © 2018 com.nafisa. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UINavigationController {
+    
+    override open var supportedInterfaceOrientations : UIInterfaceOrientationMask     {
+        
+        return [.all]
+    }
+}
+
